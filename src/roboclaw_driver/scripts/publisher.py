b233fa0910491212e227cfbcf06786b6307fbb07
scole02 <samcole107@gmail.com>
2022-02-06 13:43:02 -0800

    motors topic msg now includes: [addr, chan, cnts_per_rev, rads, rad_precision]

--- src/roboclaw_driver/scripts/publisher.py
+++ src/roboclaw_driver/scripts/publisher.py
@@ -7,10 +7,17 @@ def talker():
     pub = rospy.Publisher('motors', Int32MultiArray, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    address = int(input("enter address in base 10 (default: 128): "))
+    channel = int(input("enter motor channel: "))
+    cnts_per_rev = int(input("enter encoder counts per revolution: "))
+    rads_precision = 10 ** int(input("enter floating point position precision (0-9): "))  
     while not rospy.is_shutdown():
-        val = input("enter position in encoder counts: ")
+        rads = int(float(input("enter position in radians: ")) * rads_precision)
+        if rads < 0 or rads/rads_precision > 6.29:
+            rospy.loginfo("please enter a valid position in radians (0 to 6.283184...)") 
+            continue
         arr_msg = Int32MultiArray()
-        arr_msg.data = [0, 0, int(val)]
+        arr_msg.data = [address, channel, cnts_per_rev, rads, rads_precision]
         pub.publish(arr_msg)
         rate.sleep()
 
--- src/roboclaw_driver/scripts/subscriber.py
+++ src/roboclaw_driver/scripts/subscriber.py
@@ -2,23 +2,43 @@
 import rospy
 import time
 from roboclaw_python.roboclaw_3 import Roboclaw
+from math import pi
 
 from std_msgs.msg import String, Int32MultiArray
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
+    # fetch data from msg
+    address = data.data[0] # default: 0x80 or 128
+    channel = data.data[1]
+    cnts_per_rev = data.data[2]
+    rads = data.data[3]
+    rads_precision = data.data[4]
     #Linux comport name
     rc = Roboclaw("/dev/ttyACM0",115200)
 
     rc.Open()
-    address = 0x80
-    accel = 0
-    speed = 0
-    deccel = 0
+    buf = 1
+    accel1 = 0
+    speed1 = 0
+    deccel1 = 0
+    cnts1 = 0
+    accel2 = 0
+    speed2 = 0
+    deccel2 = 0
+    cnts2 = 0
     
-    rc.SpeedAccelDeccelPositionM1(address,accel,speed,deccel,data.data[2],1)
-
+    if channel == 1:
+        cnts1 = int((rads*cnts_per_rev) / (2*pi*rads_precision))
+    elif channel == 2:
+        cnts2 = int((rads*cnts_per_rev) / (2*pi*rads_precision))
+    else:
+        rospy.loginfo(rospy.get_caller_id() + "Invalid motor channel: " + channel)
+        return  
+    rospy.loginfo(f'cnts1: {cnts1}\n cnts2: {cnts2}')
+    rc.SpeedAccelDeccelPositionM1M2(address,accel1,speed1,deccel1,cnts1,
+                                                 accel2,speed2,deccel2,cnts2,buf)
     #rc.ForwardM1(address,32)#1/4 power forward
     #time.sleep(3)
     #rc.ForwardM1(address,0)	#1/4 power forward
@@ -31,9 +51,8 @@ def listener():
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
-
     rospy.Subscriber('motors', Int32MultiArray, callback)
-    
+    rospy.loginfo('Listening...\n')
         
 
     # spin() simply keeps python from exiting until this node is stopped
